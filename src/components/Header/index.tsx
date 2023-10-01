299710d0708419ae7f28660906b75b0086f00a65
Gabriel <Gabriel.Fialho@domesticandgeneral.com>
2023-10-01 13:01:10 -0300

    feat: added header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
 export default function Header() {
-  return <div>HEADER</div>;
+  return <div className="w-screen bg-blue-900 p-4 rounded-sm">
+    <h1 className="text-2xl font-bold sm:ml-36">Assets</h1>
+  </div>;
 }
